f5b8793625d9f37ac83bed5ca2a6d02ca328721b
MaxenceBenoit <124669677+MaxenceBenoit@users.noreply.github.com>
2023-02-12 13:50:19 +0100

    il manquait le pyplot dans les import

--- mission_1.py
+++ mission_1.py
@@ -11,6 +11,7 @@
 from math import sin, cos, pi
 import numpy as np
 from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
 
 
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
